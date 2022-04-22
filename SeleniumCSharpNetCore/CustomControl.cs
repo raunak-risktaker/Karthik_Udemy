fbb998ad3cdc125325d9d17009b5e38c5c3553db
raunak-risktaker <imraunakjha@gmail.com>
2022-04-22 19:11:44 +0530

    initial commit up to combo box
    initial commit up to combo box

---
+++ SeleniumCSharpNetCore/CustomControl.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore
+{
+    public  class CustomControl : DriverHelper
+    {
+
+        public static void comboBox(string controlName, string value)
+        {
+            IWebElement ele = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            ele.Clear();
+            ele.SendKeys(value);
+
+            Driver.FindElement(By.XPath($"(//ul[@class='o-mnits'])[5]/li[text()='{value}']")).Click();
+        }
+    }
+}
---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests : DriverHelper
+    {
+        public static ChromeOptions options;
+
+        [SetUp]
+        public void Setup()
+        {
+            options = new ChromeOptions();
+            options.AddArguments("disable-infobars");
+            Driver = new ChromeDriver(@"../../../../SeleniumCSharpNetCore/Driver",options);
+            Driver.Manage().Window.Maximize();
+            Thread.Sleep(5000);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+                CustomControl.comboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
+            }
+            finally
+            {
+                Driver.Quit();
+            }
+        }
+    }
+}
