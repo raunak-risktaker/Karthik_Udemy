a829c3cbf773b7ba8e19ab9532a3ff4b2bc235f2
raunak-risktaker <imraunakjha@gmail.com>
2022-04-23 09:15:47 +0530

    added hooks file
    added hooks file

---
+++ SeleniumCSharpNetCore/Hooks/Hooks.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumCSharpNetCore.Hooks
+{
+    public  class Hooks : DriverHelper
+    {
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArguments("start-maximized");
+            option.AddArguments("--disable-gpu");
+
+            option.AddArguments("--headless");
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            Driver = new ChromeDriver(@"../../../../SeleniumCSharpNetCore/Driver", option);
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+
+        }
+    }
+}
